52db791605b68d7f3c17f5da04edfd2263a60bb5
Reshma <pathipati.reshma@gmail.com>
2022-11-16 16:48:36 +0530

    add

--- Method_Example.java
+++ Method_Example.java
@@ -5,4 +5,21 @@ public class Method_Example {
         int add = a + b;
         System.out.println(add);
     }
+    public void sub(int a , int b){
+        int sub;
+        sub = a - b;
+        System.out.println(sub);
+    }
+    public int mul(){
+        int  a , b;
+        a = 5;
+        b = 3;
+        int mul = a * b;
+        return mul;
+    }
+    public int div(int a , int b){
+        int div;
+        div = a / b;
+        System.out.println(div);
+    }
 }
